0357c40df2122f791f1e2e6b41f0dac7a1a3ba1d
pashmash372 <abhilashsivasanker@gmail.com>
2023-10-07 04:12:14 +0530

    Storing User Input #lists

--- Python Mega Course Learn Python in 60 Days, Build 20 Apps/app1/main.py
+++ Python Mega Course Learn Python in 60 Days, Build 20 Apps/app1/main.py
@@ -1,3 +1,9 @@
 user_prompt = "Enter a todo:"
-text=input(user_prompt)
-print("You entered: "+text)
+todo1 = input(user_prompt)
+todo2 = input(user_prompt)
+todo3 = input(user_prompt)
+
+todos = [todo1, todo2, todo3]
+print(todos)
+print(type(user_prompt)) # <class 'str'>
+print(type(todos)) # <class 'list'>
